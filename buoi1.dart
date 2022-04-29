c9b7fcc51a49511469f4b7954c746919384bb934
Phat <phatdroid94@gmail.com>
2022-04-29 20:32:35 +0700

    update

--- buoi1.dart
+++ buoi1.dart
@@ -2,22 +2,36 @@ void main(){
 
   // 1 : Kiểu dữ liệu
   // 2 : Khai bao biến
-  String name = "Phạm Tấn Phát";
-  int age = 28;
-  String address = "Tp HCM";
-  String phone = "123456789";
-  bool isMale = true;
-  String symbol = "\u0198";
-  // print(symbol);
-
-  Runes runes = Runes(
-      '\u{2651} \u{2652} \u{2653} \u{2654} '
-  );
-  print(String.fromCharCodes(runes));
-
-
   // age
   // address
   // phone
   // isMale
+  // String name = "Phạm Tấn Phát";
+  // int age = 28;
+  // String address = "Tp HCM";
+  // String phone = "123456789";
+  // bool isMale = true;
+  // String symbol = "\u0198";
+  // // print(symbol);
+  //
+  // Runes runes = Runes(
+  //     '\u{2651} \u{2652} \u{2653} \u{2654} '
+  // );
+  // print(String.fromCharCodes(runes));
+
+  // dynamic a = 10;
+  // a = "123";
+  // print(a.runtimeType);
+
+  //3 : Xử lý chuỗi
+  // String a = "Hello";
+  // String b = "Tôi tên là Phát";
+  // String output = a + b;
+  // print(output);
+
+  // 4 : String interpolation
+  // String name = "Phát";
+  // int age = 28;
+  // String output = "Name : ${name} , Age : ${age}";
+  // print(output);
 }
